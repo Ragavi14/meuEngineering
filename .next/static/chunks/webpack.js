cdd13d7a2085c4d030d69611fdf76bd2cd4b44c2
shashi <shashi09568@gmail.com>
2024-03-27 18:49:31 +0530

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fe3b03c7496899a"; }
+/******/ 		__webpack_require__.h = function() { return "56a614691080f5d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
