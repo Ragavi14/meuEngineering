540407d32bab660dede40c338bd4b674bb89c96e
shashi <shashi09568@gmail.com>
2024-04-15 10:59:31 +0530

    changes

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/about-company\":[],\"/product\":[],\"/facilities\":[],\"/contact\":[],\"/product-details\":[],\"/\":[]},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
