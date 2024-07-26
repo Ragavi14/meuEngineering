baadd6443c2e44930afad4fb336654405855f678
Ragavi14 <ragavi@netiapps.com>
2024-07-26 10:57:00 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <html lang="en">
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=ADLaM+Display&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet" />
       <body>{children}</body>
       </html>
