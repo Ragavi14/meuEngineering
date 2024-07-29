d754c93dbb18139381a2e163ee1b85a1ef7553e3
shashi <shashi09568@gmail.com>
2024-07-29 12:15:32 +0530

    changes

--- components/clientSection/clientSection.tsx
+++ components/clientSection/clientSection.tsx
@@ -15,6 +15,32 @@ export default function ClientSection (client: any) {
         speed: 5000,
         // autoplaySpeed: 10000,
         cssEase: "linear",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
     return(
         <div>
